cbdbd5992221c3e38dbaa3206deeb48f2be2c499
Ben Lleve <francisben.lleve@gmail.com>
2018-11-05 00:42:52 +0800

    TODO: use notify-send

--- gmail-checker.sh
+++ gmail-checker.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-mail=$( curl -u $gmail_username:$gmail_password --silent "https://mail.google.com/mail/feed/atom" |  grep -oPm1 "(?<=<title>)[^<]+" | sed '1d' )
+export DISPLAY=":0.0"
+
+mail=$( curl -u $GUSER:$GPASS --silent "https://mail.google.com/mail/feed/atom" |  grep -oPm1 "(?<=<title>)[^<]+" | sed '1d' )
 
 # https://askubuntu.com/a/92195/874085
 # https://linuxconfig.org/check-your-gmail-inbox-for-new-emails-with-bash-script
 
 if [ -n "$mail" ]; then
-
     GCOUNT=`echo "$mail" | wc -l`
 
     if [ "$GCOUNT" -gt 1 ]; then
@@ -15,12 +16,16 @@ if [ -n "$mail" ]; then
         notif_title="New Mail"
     fi
 
-    echo "GC_STR='$notif_title: $GCOUNT'" > /dev/shm/foo
+    kill -9 $(pgrep -f 'sh /home/ben/.xinitrc')
+    kill -9 $(pgrep -f 'sh /mnt/sdcard/scripts/dwm_statusbar.sh')
+    xsetroot -name "!!! $notif_title: $GCOUNT !!!"
 
-    # echo "GC_STR='$notif_title: $GCOUNT'" > /tmp/gc.sh
+    sleep 8s
 
-    # notify-send --icon=gnome-stock-mail-new "$count $notif_title" "$mail"
-    # printf "$notif_title:\n$mail"
-else
-    echo "GC_STR=':)'" > /dev/shm/foo
+    sh /mnt/sdcard/scripts/dwm_statusbar.sh
 fi
+
+# echo "GC_STR='$notif_title: $GCOUNT'" > /tmp/gc.sh
+# echo "GC_STR='$notif_title: $GCOUNT'" > /dev/shm/foo
+# notify-send --icon=gnome-stock-mail-new "$count $notif_title" "$mail"
+# printf "$notif_title:\n$mail"
