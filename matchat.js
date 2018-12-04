0fb02ad42680070aa0f67a3138c4c9c518224f9d
Ben Lleve <francisben.lleve@gmail.com>
2018-12-04 17:33:02 +0800

    bookmark

--- matchat.js
+++ matchat.js
@@ -2,3 +2,6 @@ const matchat = document.querySelector('.acp_content').children[0].src
 const matchatURL = matchat.slice(8)
 
 window.open('http://' + matchatURL)
+
+// browser bookmark
+javascript:(function(){ const matchat = document.querySelector('.acp_content').children[0].src; const matchatURL = matchat.slice(8); window.open('http://' + matchatURL) })()
